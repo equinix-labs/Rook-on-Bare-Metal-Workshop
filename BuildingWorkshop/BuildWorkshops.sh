aa4d2dbe3ded07aa2cdc5825af8fba9df43434c0
John Studarus <john@jhlconsulting.com>
2019-05-03 01:55:46 +0000

    wip

--- BuildingWorkshop/BuildWorkshops.sh
+++ BuildingWorkshop/BuildWorkshops.sh
@@ -6,10 +6,15 @@ sudo pip install -r kubespray/requirements.txt
 ssh-keygen -t ed25519 -q -N ""
 
 cp -LRp kubespray/contrib/terraform/packet/sample-inventory/ kubespray/inventory/alpha
+# need to add cd
 ln -s kubespray/contrib/terraform/packet/hosts kubespray/inventory/alpha
 cp cluster.tf kubespray/inventory/alpha/cluster.tf
 
 # export PACKET_AUTH_TOKEN="XYZ"
 
+#  need to add cd
 terraform init kubespray/contrib/terraform/packet/
 terraform apply --var-file=cluster.tf kubespray/contrib/terraform/packet/
+
+# from Kubespray/
+ansible-playbook --become -i inventory/alpha/hosts cluster.yml
